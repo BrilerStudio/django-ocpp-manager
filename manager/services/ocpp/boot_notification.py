1505bd9ddb47abdece389b3aaa08168ba926c44f
Yaros <yaros@Yaross-Laptop.local>
2024-01-28 19:44:38 +0100

    Revive charge point data

--- manager/services/ocpp/boot_notification.py
+++ manager/services/ocpp/boot_notification.py
@@ -1,6 +1,7 @@
-from ocpp.v16.enums import RegistrationStatus
+from ocpp.v16.enums import RegistrationStatus, ChargePointStatus
 
 from app.utils import get_utc_as_string
+from manager.models import ChargePoint
 from manager.ocpp_events.boot_notification import BootNotificationEvent
 from manager.ocpp_models.tasks.boot_notification import BootNotificationTask
 from utils.logging import logger
@@ -10,6 +11,18 @@ async def process_boot_notification(
         event: BootNotificationEvent
 ) -> BootNotificationTask:
     logger.info(f'Start process BootNotification (event={event})')
+
+    data = {
+        'model': event.payload.charge_point_model,
+        'serial_number': event.payload.charge_point_serial_number,
+        'manufacturer': event.payload.charge_point_vendor,
+    }
+    data = {k: v for k, v in data.items() if v}
+
+    await ChargePoint.objects.filter(charge_point_id=event.charge_point_id).aupdate(
+        status=ChargePointStatus.available,
+        **data,
+    )
 
     return BootNotificationTask(
         message_id=event.message_id,
